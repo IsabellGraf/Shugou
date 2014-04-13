90b97ee3e61577bfdda1dd99289d65486ef259ab
jerome.p.lefebvre@gmail.com <jerome.p.lefebvre@gmail.com>
2014-04-13 09:34:46 -0700

    Tried to do some basic styling, but this is hard to do in kivy. This atleast sets-up the size of the game to mimic an iPad (or what ever else your phone is)

--- screen.py
+++ screen.py
@@ -12,6 +12,62 @@ from kivy.uix.label import Label
 from Deck import Deck
 
 
+import sys
+from os import environ
+from kivy.config import Config
+from kivy.logger import Logger
+
+# taken from http://en.wikipedia.org/wiki/List_of_displays_by_pixel_density
+devices = {
+    # device: (name, width, height, dpi, density)
+    'onex': ('HTC One X', 1280, 720, 312, 2),
+    's3': ('Galaxy SIII', 1280, 720, 306, 2),
+    'droid2': ('Motolora Droid 2', 854, 480, 240, 1.5),
+    'xoom': ('Motolora Xoom', 1280, 800, 149, 1),
+    'ipad': ('iPad (1 and 2)', 1024, 768, 132, 1),
+    'ipad3': ('iPad 3', 2048, 1536, 264, 2),
+    'iphone4': ('iPhone 4', 640, 960, 326, 2),
+    'iphone5': ('iPhone 5', 640, 1136, 326, 2),
+}
+
+
+def start(win, ctx):
+    pass
+
+
+def stop(win, ctx):
+    pass
+
+
+def apply_device(device, scale, orientation):
+    name, width, height, dpi, density = devices[device]
+    if orientation == 'portrait':
+        width, height = height, width
+    Logger.info('Screen: Apply screen settings for {0}'.format(name))
+    Logger.info('Screen: size={0}x{1} dpi={2} density={3} '
+        'orientation={4}'.format(width, height, dpi, density, orientation))
+    environ['KIVY_METRICS_DENSITY'] = str(density)
+    environ['KIVY_DPI'] = str(dpi)
+    Config.set('graphics', 'width', str(width))
+    Config.set('graphics', 'height', str(height))
+    Config.set('graphics', 'fullscreen', '0')
+    Config.set('graphics', 'show_mousecursor', '1')
+
+
+def configure(ctx):
+    scale = ctx.pop('scale', None)
+    orientation = 'landscape'
+    ctx.pop('landscape', None)
+    if ctx.pop('portrait', None):
+        orientation = 'portrait'
+    if not ctx:
+        return usage(None)
+    device = ctx.keys()[0]
+    if device not in devices:
+        return usage('')
+    apply_device(device, scale, orientation)
+
+
 class MyToggleButton(ToggleButton):
     pass
 
@@ -63,6 +119,7 @@ class GameLayout(FloatLayout):
 class ScreenApp(App):
 
     def build(self):
+        apply_device('ipad', None, 'portrait')
         return GameLayout()
 
 if __name__ == '__main__':
