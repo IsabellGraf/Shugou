2230f493194026705acad9e26d9908f2462cd6e9
BernhardKonrad <bernhard.konrad@gmail.com>
2014-04-15 21:32:33 -0700

    Adds ratingList ELO-style

--- AI.py
+++ AI.py
@@ -1,10 +1,10 @@
 import random
 from Deck import Deck,Card
     
-def AI(table):   
+def AI(table):
     all_sets = Deck.allSets(table)
     set_difficulties = []
-	
+
     for the_set in all_sets:
         set_difficulties.append(get_difficulties(the_set))
 
@@ -15,9 +15,9 @@ def AI(table):
 
 
 def get_difficulties(the_set):
-    num_similar = Deck.similarities(the_set[0], the_set[1], the_set[2])
-    difficulty = 4-num_similar
-    return difficulty
+#    num_similar = Deck.similarities(the_set[0], the_set[1], the_set[2])
+#    difficulty = 4-num_similar
+    return ratingList[Deck.setIdentifier(the-set)]
     
     
 def get_time(set_difficulties):
@@ -30,7 +30,6 @@ def get_time(set_difficulties):
     time = the_time - 4*(4-difficulty)*length
     
     return time, index
-
 
 
 def demo():
@@ -54,5 +53,46 @@ def demo():
     print card2
     print card3
 
+    
+def newRatings(winnerRating, loserRating, K = 32):
+    '''calculates ratings of winner and loser according to ELO http://en.wikipedia.org/wiki/Elo_rating_system'''
+    QWinner = 10**(winnerRating/float(400))
+    QLoser = 10**(loserRating/float(400))
+    EWinner = QWinner/(QWinner+QLoser)
+    ELoser = 1-EWinner
+    newWinnerRating = winnerRating + K*(1-EWinner)
+    newLoserRating = loserRating + K*(0-ELoser)
+    return newWinnerRating, newLoserRating
+
+        
+def updateRatings(table, setFound):
+    '''updates the ratings of all sets involved in this round
+    
+    Please call
+    >> updateRatings(table, setFound)
+    whenever a human finds a set
+    '''
+    initialRating = 1500 #any number would work, chess uses something like this usually, so why not...
+    setFoundKey = Deck.setIdentifier(setFound)
+    try:
+        setFoundRating = ratingList[setFoundKey]
+    except KeyError: #first time this set shows up
+        setFoundRating = initialRating
+    
+    losingSets = Deck.allSets(table)-setFound
+    for the_set in losingSets:
+        losingSetKey = Deck.setIdentifier(the_set)
+        try:
+            losingSetRating = ratingList[losingSetKey]
+        except KeyError: #first time this set shows up
+            losingSetRating = initialRating
+        newWinnerRating, newLoserRating = newRatings(setFoundRating, losingSetRating)
+        ratingList[setFoundKey] = newWinnerRating
+        ratingList[losingSetKey] = newLoserRating
+        
+# does this update ratingList automatically?
+        
+    
+    
 if __name__ == "__main__":
     demo()
