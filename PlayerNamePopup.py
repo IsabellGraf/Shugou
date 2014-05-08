3c71efe7165df635f9172a58cfcafd27e337410c
jerome.p.lefebvre@gmail.com <jerome.p.lefebvre@gmail.com>
2014-05-08 14:49:53 -0700

    Final playing around with PlayerNamePopup

--- PlayerNamePopup.py
+++ PlayerNamePopup.py
@@ -1,9 +1,6 @@
-
 from kivy.uix.popup import Popup
-from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
-from kivy.uix.gridlayout import GridLayout
-from kivy.properties import *
+from kivy.properties import StringProperty
 
 class PlayerNameEdit(Popup):
     nameText = StringProperty('')
@@ -16,19 +13,16 @@ class PlayerNamePopup(Popup):
     def __init__(self, namesOfPlayers):
         super(PlayerNamePopup, self).__init__()
         self.namesOfPlayers = namesOfPlayers
-        # Create the screen which allows to select which user's name to change
+        # Create the screen which allows to select which users' name to change
         for i, name in enumerate(self.namesOfPlayers):
-            button = Button(text=name)
+            button = Button(text=name, on_press=self.click)
             button.value = i
-            button.bind(on_press=self.click)
             self.ids.content.add_widget(button)
 
     def click(self, button):
-        # In here, we create the popup where we request the user's names.
-        # On click of the name we want to change, the user can enter a new
-        # name.
+        # In here, we create the popup where we request the new user's names.
         playerdit = PlayerNameEdit(nameText = button.text, 
-                                   on_dismiss=lambda popup: 
+                                   on_dismiss = lambda popup: 
                                             self.set_caption(popup.ids.textinput.text, button))
         playerdit.ids.textinput.select_all()
         playerdit.open()
