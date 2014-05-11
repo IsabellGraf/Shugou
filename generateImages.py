18573c69184a8f94aac177586734ee129c7464a5
jiag <jiaaguu@gmail.com>
2014-05-10 21:36:39 -0700

    create rounded corner images for buttons

--- generateImages.py
+++ generateImages.py
@@ -1,10 +1,12 @@
 # To run the file you must first install the Pillow module
 # i.e. pip install Pillow or pip3 install Pillow
+# need install cairo module
+# brew install cairo, brew install py2cairo(for python2) or pycairo(for python3)
 
 from PIL import ImageFont, Image, ImageDraw
 
 from Deck import Deck
-
+import cairo
 
 def make_star(infos, size):
     d = infos[1]
@@ -44,6 +46,61 @@ def make_ellipse(infos, size):
     xx = [(d-a,c-a), (d+a,c+a)]
     return xx
 
+def draw_rounded(cr, area, radius, fillin, *argv):
+    """ draws rectangles with rounded (circular arc) corners """
+    from math import pi
+    a,b,c,d=area
+    cr.arc(a + radius, c + radius, radius, 2*(pi/2), 3*(pi/2))
+    cr.arc(b - radius, c + radius, radius, 3*(pi/2), 4*(pi/2))
+    cr.arc(b - radius, d - radius, radius, 0*(pi/2), 1*(pi/2))  # ;o)
+    cr.arc(a + radius, d - radius, radius, 1*(pi/2), 2*(pi/2))
+    cr.close_path()
+    if fillin == 'Fill':
+        cr.set_source_rgba(*argv)
+        cr.fill()
+    else:
+        cr.set_line_width(5)
+        cr.set_source_rgba(*argv)
+        cr.stroke()
+
+
+def draw_contour(w,h,state):
+    import cairo
+    offset = 10
+    wid = 35
+    if state == 'Normal':
+        fig_size = (w+offset,h+offset+wid)
+    else:
+        fig_size = (w+offset,h+offset)
+
+    # an area with coordinates of
+    # (top, bottom, left, right) edges in absolute coordinates:
+    inside_area = (offset, w-offset, offset, h-offset)
+
+    surface = cairo.ImageSurface(cairo.FORMAT_ARGB32, *fig_size)
+    cr = cairo.Context(surface)
+
+    if state == 'Normal':
+        colorcode = (101/255.,173/255.,178/255.,1)
+        background_color = (1.,1.,1.,1.)
+        inside_area_down = (offset, w-offset, offset, h-wid)
+
+        draw_rounded(cr, inside_area, 40, 'Fill', *colorcode)
+        draw_rounded(cr, inside_area, 40,'Border', *colorcode)
+
+        draw_rounded(cr, inside_area_down, 40,'Fill', *background_color)
+        draw_rounded(cr, inside_area_down, 40,'Border', *colorcode)
+
+    elif state == 'Down':
+        background_color = (255/255.,200/255.,200/255.,1.)
+        colorcode = (101/255.,173/255.,178/255.,1)
+
+        draw_rounded(cr, inside_area, 40, 'Fill', *background_color)
+        draw_rounded(cr, inside_area, 40,'Border', *colorcode)
+
+    im = Image.frombuffer("RGBA", fig_size, surface.get_data(), "raw", "BGRA", 0,1)
+    return im
+
 
 white = (255, 255, 255)
 red = (255, 200, 200)
@@ -68,8 +125,10 @@ numbers = {1: [[double * 20, double * 31, double * 19]],
 deck = Deck()
 
 for card in deck:
-    im1 = Image.new('RGB', (double * 62, double * 38), white)  # normal state
-    im2 = Image.new('RGB', (double * 62, double * 38), red)  # down state
+    #im1 = Image.new('RGB', (double * 62, double * 38), white)  # normal state
+    #im2 = Image.new('RGB', (double * 62, double * 38), red)  # down state
+    im1 = draw_contour(double * 62, double * 38, 'Normal')
+    im2 = draw_contour(double * 62, double * 38, 'Down')
     draw1 = ImageDraw.Draw(im1)
     draw2 = ImageDraw.Draw(im2)
 
@@ -121,5 +180,6 @@ for card in deck:
                 draw2.polygon(shape(infovec, size), fill=color)
 
     # saves the images where normalimage and downimage is pointing to
+    # dpi sets the resolution of images(dots per inch)
     im1.save(card.normalimage(), dpi=(264,264))
     im2.save(card.downimage(), dpi=(264,264))
